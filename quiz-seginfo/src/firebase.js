10dfdfbbc2feb16c39193fbb7abf5071cbe6db06
mercurysr <108282925+mercurysr@users.noreply.github.com>
2025-09-03 10:35:10 -0300

    Update firebase.js

--- quiz-seginfo/src/firebase.js
+++ quiz-seginfo/src/firebase.js
@@ -10,13 +10,7 @@ import { createUserWithEmailAndPassword } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAia-dXWuFeTjc7wjqjYFnJcOcxNu6OHvA",
-  authDomain: "game-si.firebaseapp.com",
-  projectId: "game-si",
-  storageBucket: "game-si.firebasestorage.app",
-  messagingSenderId: "1019804807994",
-  appId: "1:1019804807994:web:a4d5bf9eec78e8e4705603",
-  measurementId: "G-RSX469ZSSN"
+ //aqui fica os parametros da chave api do nosso projeto pega comigo no wpp
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
